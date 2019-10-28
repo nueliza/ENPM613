670a9f0ef5d22ab326a381be3db40671cbd5fc63
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-10-28 13:48:28 -0400

    Created Exam State. Options peding

--- src/components/exams/createExam.js
+++ src/components/exams/createExam.js
@@ -7,40 +7,70 @@ class CreateExam extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            Exam: {}
+            Exam: [{ question: "Type Question Here", options: {}, answer: "Type Answer here" }]
         }
     }
 
-    dropdownChange(e){
-        if(e.target.value=="checkbox"){
-        }
+    addQuestion = (e) =>{
+        this.setState((prevState)=>({
+            Exam:[...prevState.Exam, { question: "Type Question Here", options: {}, answer: "Type Answer here" }]
+        }));
     }
+    
+    handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+
+    handleChange = (e) =>{
+        let Exam = [...this.state.Exam]
+        Exam[e.target.dataset.id][e.target.className] = e.target.value;
+        this.setState({Exam}, ()=>console.log("CreateExam Page: State Updated", this.state.Exam));
+
+    }
+    
     render() {
+        let { Exam } = this.state;
         //<h3>Create {this.props.location.state.selectedSubModule} Exam</h3>
-        console.log(`CreateExam Page: ${Object.keys(this.state.Exam).length}`)
-        console.log("CreateExam Page:" ,this.props)
         return (
             <Fragment>
                 <h3>Algebra</h3>
-                <div className="questionWrapper">
-                    Question number {Object.keys(this.state.Exam).length + 1}
-                    <br />
-                    <input type="text" placeholder="Type Question here" />
-                    <br />
-                    <div className="optionsWrapper">
-                        <input type="text" placeholder="Option 1" /> 
-                        <input type="text" placeholder="Option 2" />
-                        <br/>
-                        The correct answer:<br/>
-                         <input type="text" placeholder="Type the correct answer here"/>
-                    </div>
-                </div>
-                
-                
-
-
-
-            </Fragment>)
+                <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+                    {Exam.map((item, id) => {
+                        let questionId = `qn-${id}`, answerId = `ans-${id}`
+                        return (
+                            <div className="questionWrapper" key={id}>
+                                Question number {id+1}
+                                <input type="text"
+                                    name={questionId}
+                                    data-id={id}
+                                    id={questionId}
+                                    className="question"
+                                    defaultValue={Exam[id].question} 
+                                />
+                                <br />
+                                Options
+                                <div className="optionsWrapper">
+                                    <input type="text" />
+                                    <input type="text" />
+                                </div>
+                                Correct Answer
+                                <input type="text" 
+                                    name={answerId}
+                                    data-id={id}
+                                    id={answerId}
+                                    className="answer"
+                                    defaultValue={Exam[id].answer}
+                                />
+                            </div>
+                        )
+                    })}
+                    <button onClick= {this.addQuestion}>
+                        <FontAwesomeIcon icon={iconMapping["Plus"]} size="1x" />
+                        &nbsp;Add another Question
+                    </button>
+                </form>
+            </Fragment>
+        )
     }
 }
 export default CreateExam;
