f5c1127403eb7cfc8839e8b257274df12c72bb66
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-10-31 13:24:25 -0400

    set up reducer and actions for exams, discussions and dashboard

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import authenticationReducer from './authenticationReducer';
 import moduleReducer from "./moduleReducer";
+import examReducer from "./examReducer";
+import discussionReducer from "./discussionReducer";
+import dashboardReducer from "./dashboardReducer";
 
 export default combineReducers({
     user : authenticationReducer ,
-    modules : moduleReducer 
+    modules : moduleReducer ,
+    exam : examReducer,
+    discussion: discussionReducer,
+    dashboard: dashboardReducer
 })
--- src/reducers/moduleReducer.js
+++ src/reducers/moduleReducer.js
@@ -1,15 +1,26 @@
 const initialState = {
-    selectedModule: ''
+    selectedModule: '',
+    userInfo: {},
+    registerMessage: {error: false, message: ""}
 }
 
-const authenticationReducer = (state = initialState, action) => {
-switch (action.type) {
-    case 'SET_SELECTED_MODULE':
-    return {
-        ...state,selectedModule: action.data }
-    default:
-    return state
+const moduleReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_SELECTED_MODULE':
+            return {
+                ...state, selectedModule: action.data
+            };
+        case 'LOGIN_USER':
+            return {
+                ...state, userInfo: action.data
+            };
+        case 'REGISTER_USER':
+            return {
+                ...state, registerMessage: action.data
+            };
+        default:
+            return state
     }
 }
 
-export default authenticationReducer;
+export default moduleReducer;
