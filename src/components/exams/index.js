1f74c23ac34720655c1b8dae7b29359e69dce305
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-10-31 20:49:30 -0400

    updated exam.js

--- src/components/exams/index.js
+++ src/components/exams/index.js
@@ -5,49 +5,95 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Exams extends Component {
     render() {
-        const isTutor = this.props.userInfo.type == "Tutor"? true : false;
+        const isTutor = this.props.userInfo.type == "Tutor" ? true : false;
         return (
             <Fragment>
                 <div className="dashboard_body">
                     <div className="dashboard_subSection">
                         <h2> Algebra</h2>
                         <ul className="list-group">
-                            <li className="list-group-item">Exam 1 <span className="tag newTag"><b>New</b></span><button type="button" className="btn btn-info">Take Exam</button></li>
-                            <li className="list-group-item">Exam 2 <button type="button" className="btn btn-info">Take Exam</button></li>
-                            <li className="list-group-item">Exam 3 <button type="button" className="btn btn-info">Take Exam</button></li>
-                            <li className="list-group-item">Exam 4 <span className="tag notPublishedTag"><FontAwesomeIcon icon={iconMapping["NotPublished"]} size="1x" />&nbsp;<b>Not published</b></span><button type="button" className="btn btn-secondary">Take Exam</button></li>
+                            <li className="list-group-item">Exam 1
+                                <span className="tag newTag">
+                                    <b>New</b>
+                                </span>
+                                <button type="button" className="btn btn-info">
+                                    Take Exam
+                                </button>
+                            </li>
+                            <li className="list-group-item">Exam 2
+                                <button type="button" className="btn btn-info">
+                                    Take Exam
+                                </button>
+                            </li>
+                            <li className="list-group-item">Exam 3
+                                 <button type="button" className="btn btn-info">
+                                    Take Exam
+                                </button>
+                            </li>
+                            <li className="list-group-item">Exam 4 
+                                <span className="tag notPublishedTag">
+                                    <FontAwesomeIcon icon={iconMapping["NotPublished"]} size="1x" />&nbsp;
+                                    <b>Not published</b>
+                                </span>
+                                <button type="button" className="btn btn-secondary">
+                                    Take Exam
+                                </button>
+                            </li>
                         </ul>
-                        {isTutor? 
-                        <button type="button" className="btn btn-link" onClick={() => {
-                            this.props.history.push({
-                                pathname: '/CreateExam',
-                                state: { selectedSubModule: 'Algebra'}
-                            })
-                        }}>
-                            <FontAwesomeIcon icon={iconMapping["Plus"]} size="1x" />
-                            &nbsp;Add Exam
+                        {isTutor ?
+                            <button type="button" className="btn btn-link" onClick={() => {
+                                this.props.history.push({
+                                    pathname: '/CreateExam',
+                                    state: { selectedSubModule: 'Algebra' }
+                                })
+                            }}>
+                                <FontAwesomeIcon icon={iconMapping["Plus"]} size="1x" />
+                                &nbsp;Add Exam
                         </button>
-                        : '' }
+                            : ''}
                     </div>
                     <div className="dashboard_subSection">
-                        <h2> Calculus</h2>
+                        <h2> Algebra</h2>
                         <ul className="list-group">
-                            <li className="list-group-item">Exam 1 <span className="tag newTag"><b>New</b></span><button type="button" className="btn btn-info">Take Exam</button></li>
-                            <li className="list-group-item">Exam 2 <button type="button" className="btn btn-info">Take Exam</button></li>
-                            <li className="list-group-item">Exam 3 <button type="button" className="btn btn-info">Take Exam</button></li>
-                            <li className="list-group-item">Exam 4 <span className="tag notPublishedTag"><FontAwesomeIcon icon={iconMapping["NotPublished"]} size="1x" />&nbsp;<b>Not published</b></span><button type="button" className="btn btn-secondary">Take Exam</button></li>
+                            <li className="list-group-item">Exam 1
+                                <span className="tag newTag">
+                                    <b>New</b>
+                                </span>
+                                <button type="button" className="btn btn-info">
+                                    Take Exam
+                                </button>
+                            </li>
+                            <li className="list-group-item">Exam 2
+                                <button type="button" className="btn btn-info">
+                                    Take Exam
+                                </button>
+                            </li>
+                            <li className="list-group-item">Exam 3
+                                 <button type="button" className="btn btn-info">
+                                    Take Exam
+                                </button>
+                            </li>
+                            <li className="list-group-item">Exam 4 
+                                <span className="tag notPublishedTag">
+                                    <FontAwesomeIcon icon={iconMapping["NotPublished"]} size="1x" />&nbsp;
+                                    <b>Not published</b>
+                                </span>
+                                <button type="button" className="btn btn-secondary">
+                                    Take Exam
+                                </button>
+                            </li>
                         </ul>
-                        {isTutor? 
-                        <button type="button" className="btn btn-link" onClick={() => {
-                            this.props.history.push({
-                                pathname: '/CreateExam',
-                                state: { selectedSubModule: 'Calculus'}
-                            })
-                        }}>
-                            <FontAwesomeIcon icon={iconMapping["Plus"]} size="1x" />
-                            &nbsp;Add Exam
+                        {isTutor ?
+                            <button type="button" className="btn btn-link" onClick={() => {
+                                this.props.history.push({
+                                    pathname: '/CreateExam',
+                                    state: { selectedSubModule: 'Algebra' }
+                                })
+                            }}>
+                                <FontAwesomeIcon icon={iconMapping["Plus"]} size="1x" />
+                                &nbsp;Add Exam
                         </button>
-                        : '' }
+                            : ''}
                     </div>
                 </div>
 
