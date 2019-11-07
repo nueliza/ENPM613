e15090fe80a8b1d08a19d4567ba54cea77ba855c
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-11-06 22:09:57 -0500

    quotes

--- client/src/components/files/index.js
+++ client/src/components/files/index.js
@@ -8,25 +8,12 @@ class Files extends Component {
         return (
             <div className="dashboard_body files_body">
                 <div className="dashboard_subSection">
-                    <h2>Algebra</h2>
-                    <ul className="list-group">
-                        <li className="list-group-item">File 1 <button type="button" className="btn btn-info">Download</button></li>
-                        <li className="list-group-item">File 2 <button type="button" className="btn btn-info">Download</button></li>
-                        <li className="list-group-item">File 3 <button type="button" className="btn btn-info">Download</button></li>
-                        <li className="list-group-item">File 4 <span className="tag notPublishedTag"><FontAwesomeIcon icon={iconMapping["NotPublished"]} size="1x" />&nbsp;<b>Not published</b></span><button type="button" className="btn btn-secondary">Download</button></li>
-                    </ul>
-                </div>
-                <div className="dashboard_subSection">
-                    <h2>Calculus</h2>
-                    <ul className="list-group">
-                        <li className="list-group-item">File 1 <button type="button" className="btn btn-info">Download</button></li>
-                        <li className="list-group-item">File 2 <button type="button" className="btn btn-info">Download</button></li>
-                        <li className="list-group-item">File 3 <button type="button" className="btn btn-info">Download</button></li>
-                        <li className="list-group-item">File 4 <span className="tag notPublishedTag"><FontAwesomeIcon icon={iconMapping["NotPublished"]} size="1x" />&nbsp;<b>Not published</b></span><button type="button" className="btn btn-secondary">Download</button></li>
-                    </ul>
-                </div>
-                <div className="dashboard_subSection">
-                    <h2>Calculus</h2>
+                    <div className="quoteWrapper">
+                        <FontAwesomeIcon icon={iconMapping["QuoteLeft"]} size="2x" style={{color: "gray"}} />&nbsp;
+                        <span className="quoteContent">An investment in knowledge pays the best interest </span>
+                        <span className="author">- Benjamin Franklin</span>
+                    </div>
+                    <br />
                     <ul className="list-group">
                         <li className="list-group-item">File 1 <button type="button" className="btn btn-info">Download</button></li>
                         <li className="list-group-item">File 2 <button type="button" className="btn btn-info">Download</button></li>
--- client/src/components/grades/index.js
+++ client/src/components/grades/index.js
@@ -1,4 +1,6 @@
 import React, { Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { iconMapping } from "../utils/iconsMapping.js";
 import "./grades.css";
 
 class Grades extends Component {
@@ -7,6 +9,12 @@ class Grades extends Component {
         return (
             <div className="dashboard_body grades_body">
                 <div className="dashboard_subSection">
+                    <div className="quoteWrapper">
+                        <FontAwesomeIcon icon={iconMapping["QuoteLeft"]} size="2x" style={{color: "gray"}} />&nbsp;
+                        <span className="quoteContent">Perpetual optimism is a force multiplier </span>
+                        <span className="author">- Unknown</span>
+                    </div>
+                    <br />
                     <ul className="list-group">
                         <li className="list-group-item">Exam 1 <span className="Score"> Score:  23</span><button type="button" className="btn btn-info">View Answers</button></li>
                         <li className="list-group-item">Exam 1 <span className="Score"> Score:  23</span><button type="button" className="btn btn-info">View Answers</button></li>
