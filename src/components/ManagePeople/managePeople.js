54f8e9d88a9b974847a6fb16a544f49520e8c1f3
Arrow43 <31423564+Arrow43@users.noreply.github.com>
2019-11-25 16:48:09 -0500

    Added comments for what needs to be done

--- src/components/ManagePeople/managePeople.js
+++ src/components/ManagePeople/managePeople.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 class ManagePeople extends Component {
     render(){
-        return (<div> ManagePeople !!!! </div>)
+        return (
+            //Call API to get list of students
+            //Call API to get list of tutors
+            //Add remove student button next to each student
+            //Connect API call to delete a student with each remove button
+            //Implement secondary login before all of this functionality
+            <div> 
+                ManagePeople !!!! 
+            </div>
+        )
     }
 }
 
 export default ManagePeople;
-
