e7f1677eed4bf013424571dabd4434a88b443b69
Arrow43 <31423564+Arrow43@users.noreply.github.com>
2019-11-25 19:52:27 -0500

    Update managePeople.js

--- src/components/ManagePeople/managePeople.js
+++ src/components/ManagePeople/managePeople.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { getStudentList } from '../../actions/studentHandler';
 
 class ManagePeople extends Component {
     render(){
+        //Call API to get list of student.
+        //Call API to check whether login is sucessfull and update store.
+        //Call API to get list of tutors
+        //Add remove student button next to each student
+        //Connect API call to delete a student with each remove button
+        //Implement secondary login before all of this functionality
         return (
-            //Call API to get list of students
-            //Call API to get list of tutors
-            //Add remove student button next to each student
-            //Connect API call to delete a student with each remove button
-            //Implement secondary login before all of this functionality
             <div> 
-                ManagePeople !!!! 
+                Manage People
             </div>
         )
     }
