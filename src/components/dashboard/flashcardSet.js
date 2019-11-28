ed132fd088677596ae9abdabfbf124577fee5292
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-11-28 17:06:20 -0500

    Flashcard sets changes

--- src/components/dashboard/flashcardSet.js
+++ src/components/dashboard/flashcardSet.js
@@ -2,10 +2,25 @@ import React, { Component, Fragment } from 'react';
 import { withRouter } from "react-router-dom";
 import Loading from "../loading";
 import NotFound from "../NotFound";
+import { iconMapping } from "../utils/iconsMapping.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './dashboard.css';
 
 class FlashcardSet extends Component {
     componentDidMount() {
         this.props.getFlashcardSets({"mod_id": this.props.selectedModuleId})
+    }
+
+    handleReset = () =>{
+        //TODO
+        // let payload = {
+        //     "set_id": this.props.flashcard.set_id
+        // }
+        // this.props.resetProgress(payload)
+        // .then(()=>{
+        //     this.props.getFlashcard({"set_id": this.props.flashcard.set_id})
+        // })
     }
 
     render() {
@@ -14,17 +29,34 @@ class FlashcardSet extends Component {
         return Object.keys(this.props.flashcardSets).length === 0 ? <NotFound /> :
          (
             <Fragment>
-                <div className="modules">
+                <div className="modules flashcardSetDashboard">
                     {this.props.flashcardSets.map((set, id)=>{
                         return(
-                            <div className="card" key={id} onClick={() => {
-                                this.props.getFlashcard({"set_id": set.set_id})
-                                this.props.history.push({
-                                    pathname: '/flashcards',
-                                })
-                            }}>
+                            <div className="card" key={id} >
                                 <div className="card-body">
                                     <h5 className="card-title">{set.set_name}</h5>
+                                    <div className="progress" style={{marginBottom: "10px"}}>
+                                        <div className="progress-bar" style={{ width: "50%", backgroundColor: 'var(--primary-color)' }}><b>50%</b></div>
+                                    </div>
+                                       <div>
+                                        <button className="btn btn-info getSatProSecondaryButton" onClick={() => {
+                                            this.props.getFlashcard({"set_id": set.set_id})
+                                            this.props.history.push({
+                                                pathname: '/flashcards',
+                                            })
+                                        }}>
+                                            <FontAwesomeIcon icon={iconMapping["play"]} size="1x" /> &nbsp;
+                                            Get started
+                                        </button>
+                                        <button className="btn btn-info getSatProSecondaryButton"
+                                        style={{marginLeft: "10px"}}
+                                         onClick={() =>{
+                                            this.handleReset()
+                                        }}>
+                                            <FontAwesomeIcon icon={iconMapping["redo"]} size="1x" /> &nbsp;
+                                            Reset Progress 
+                                        </button>
+                                        </div>
                                 </div>
                             </div>
                         )
--- src/components/flashcards/index.js
+++ src/components/flashcards/index.js
@@ -2,18 +2,12 @@ import React, { Component, Fragment } from 'react';
 import CardContainer from './cardConatiner';
 import Loading from "../loading";
 import { Redirect } from 'react-router';
+import { withRouter } from "react-router-dom";
+import { iconMapping } from "../utils/iconsMapping.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Flashcards extends Component {
-    handleReset = () =>{
-        //TODO
-        // let payload = {
-        //     "set_id": this.props.flashcard.set_id
-        // }
-        // this.props.resetProgress(payload)
-        // .then(()=>{
-        //     this.props.getFlashcard({"set_id": this.props.flashcard.set_id})
-        // })
-    }
+    
 
     render() {
         console.log("this.props", this.props)
@@ -22,13 +16,20 @@ class Flashcards extends Component {
         return this.props.flashcard === "" ? <Redirect to="/dashboard" />:
         (
             <Fragment>
-                <button className="btn btn-info getSatProSecondaryButton" onClick={() =>{
-                    this.handleReset()
-                }}>
-                    Reset Progress 
+                <button
+                    type="button"
+                    className="btn btn-info getSatProSecondaryButton"
+                    style={{marginRight: "10px"}}
+                    onClick={() => {
+                        this.props.history.push({
+                            pathname: '/dashboard',
+                        })
+                    }}>
+                    <FontAwesomeIcon icon={iconMapping["back"]} size="1x" />
+                    &nbsp;<span>Back to Flashcard Sets</span>
                 </button>
                 <CardContainer flashcard={this.props.flashcard} setPreference={this.props.setPreference} />
             </Fragment>)
     }
 }
-export default Flashcards;
+export default withRouter(Flashcards);
