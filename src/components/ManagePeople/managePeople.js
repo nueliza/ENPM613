ac8f627ae0fde66032c24c373e72cc085524da74
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-12-02 11:46:01 -0500

    Update managePeople.js

--- src/components/ManagePeople/managePeople.js
+++ src/components/ManagePeople/managePeople.js
@@ -17,13 +17,12 @@ class ManagePeople extends Component {
         this.props.logoutUser();
     }
 
-    handleDelete = (user_id, user_type) => {
+    handleDelete = (user_id) => {
         let reqObject = {
-            "model_name": "User",
+            "model_name": "Student",
             "model_id": user_id
         }
-        console.log("***************************",user_id)
-         this.props.deleteUser(reqObject)
+        this.props.deleteUser(reqObject)
     }
 
     UNSAFE_componentWillMount(){
@@ -33,12 +32,6 @@ class ManagePeople extends Component {
 
     render(){
         
-            //DONE Call API to get list of student.
-        //Call API to check whether login is sucessfull and update store.
-        //DONE Call API to get list of tutors
-            //DONE add delete button to each student
-        //Connect API call to delete a student with each delete button
-        //Implement secondary login before all of this functionality
         if(this.props.loading) return <Loading />
 
         //redirects to Not found page if the getStudentList API fails
@@ -51,40 +44,44 @@ class ManagePeople extends Component {
             <div className="dashboard_body student_body">
                 <div className="dashboard_header">
                     <h3> Manage People </h3>
-                    <div className="quoteWrapper">
-                        <FontAwesomeIcon icon={iconMapping["QuoteLeft"]} size="2x" style={{color: "gray"}} />&nbsp;
-                        <span className="quoteContent">Look around you, and all you will see are people the world would be better off without. </span>
-                        <span className="author">- Light Yagami</span>
-                    </div>
+                   
                     <div className="userInfo">
                         <span className="bold">Hello, {this.props.userInfo.fname} {this.props.userInfo.lname} !</span> <br />
                         <span>Last logged In:</span><span className="bold">{this.props.userInfo.last_logged_in}</span> <br />
                         <a href="/" onClick={this.logout}>Sign out</a>
                     </div>
+                    
                 </div>
+                <hr/>
+                <div className="quoteWrapper">
+                        <FontAwesomeIcon icon={iconMapping["QuoteLeft"]} size="2x" style={{color: "gray"}} />&nbsp;
+                        <span className="quoteContent">Look around you, and all you will see are people the world would be better off without. </span>
+                        <span className="author">- Light Yagami</span>
+                </div>
+                <br />
                 <h2>Student List</h2>
                 <div className="dashboard_subSection">
                 <ul className="list-group">
                     {this.props.studentList.map((student, id) => {
                         return <li className="list-group-item" key={id}>
-                             <div className="avatar">
-                                {student.fname.charAt(0)}{student.lname.charAt(0)}
-                            </div>
-                            &nbsp; &nbsp;
-                            <span>
-                                {student.fname} {student.lname}
-                            </span>
-                            <FontAwesomeIcon
-                            icon={iconMapping["Trash"]}
-                            size="1x"
-                            // className={props.isTutor ? "" : "hide"}
-                            style={{ color: "var(--alert-color)", float: "right", marginLeft: "10px" }}
-                            onClick={() => this.handleDelete(student.user_id)}
-                        />
-                            {/* <button onClick={this.deleteStudent} type="button" className="btn btn-info">Delete</button> */}
-                        </li>
+                                    <div className="avatar">
+                                        {student.fname.charAt(0)}{student.lname.charAt(0)}
+                                    </div>
+                                    &nbsp; &nbsp;
+                                    <span>
+                                        {student.fname} {student.lname}
+                                    </span>
+                                    <FontAwesomeIcon
+                                    icon={iconMapping["Trash"]}
+                                    size="1x"
+                                    // className={props.isTutor ? "" : "hide"}
+                                    style={{ color: "var(--alert-color)", float: "right", marginLeft: "10px" }}
+                                    onClick={() => this.handleDelete(student.user_id)}
+                                />
+                            </li>
                     })}
                 </ul>
+                <br/>
                 <h2>Tutor List</h2>
                 <ul className="list-group">
                     
@@ -97,14 +94,6 @@ class ManagePeople extends Component {
                             <span>
                                 {tutor.fname} {tutor.lname}
                             </span>
-                            <FontAwesomeIcon
-                            icon={iconMapping["Trash"]}
-                            size="1x"
-                            // className={props.isTutor ? "" : "hide"}
-                            style={{ color: "var(--alert-color)", float: "right", marginLeft: "10px" }}
-                            // onClick={() => this.handleDelete(tutor.user_id)}
-                        />
-                            {/* <button onClick={this.deleteStudent} type="button" className="btn btn-info">Delete</button> */}
                         </li>
                     })}
                 </ul>
